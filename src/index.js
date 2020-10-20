2d8e3824604f163dc612e85b15aecef75676e0d5
leealexeyvl <leealexey_94@mail.ru>
2020-10-20 08:42:39 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -36,10 +36,33 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+    function decode(expr) {
+        let currentletter = '';
+        let result = [];
+        for (let i = 0; i < expr.length; i += 10) {
+    
+        let currentCode = expr.substring(i, i + 10);
+    
+        if (currentCode == '**********') {
+                result.push(' ');
+        } else {
+    
+                    for (let j = 0; j <= currentCode.length; j += 2) {
+                        if (currentCode.substring(j, j + 2) == '10') {
+                            currentletter += '.';
+                        }  else if (currentCode.substring(j, j + 2) == '11') {
+                            currentletter += '-';
+                        } else {
+                            currentletter += '';
+                        }
+                    }
+                    result.push(MORSE_TABLE[currentletter]);
+                    currentletter = '';
+                }
+            }
+            return result.join('');
+    }
 
-function decode(expr) {
-    // write your solution here
-}
 
 module.exports = {
     decode
